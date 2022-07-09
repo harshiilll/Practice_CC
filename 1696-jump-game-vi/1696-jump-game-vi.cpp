ad64824dba74d0f9a20e97d798c648b66d371eec
Harshill Kumar Dhanjal <90548564+harshiilll@users.noreply.github.com>
2022-07-09 23:20:40 +0530

    Time: 296 ms (55.20%), Space: 215.2 MB (5.05%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        deque<pair<int, int>> dp;
+        dp.emplace_front(nums[0], 0);
+        for (int i = 1; i < nums.size(); i++) {
+            while (dp.front().second + k < i) {
+                dp.pop_front();
+            }
+            int cost = dp.front().first + nums[i];
+            while (!dp.empty() && cost >= dp.back().first) {
+                dp.pop_back();
+            }
+            dp.emplace_back(cost, i);
+        }
+        return dp.back().first;
+    }
+};
