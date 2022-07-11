c81ca5030a42dbb4fc4c06ffdd0727f38f85f468
Harshill Kumar Dhanjal <90548564+harshiilll@users.noreply.github.com>
2022-07-12 01:23:55 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -26,62 +26,33 @@ struct Job
 class Solution 
 {
     public:
-      bool static comparison(Job a, Job b) {
-         return (a.profit > b.profit);
-      }
-   //Function to find the maximum profit and the number of jobs done
-   vector<int> JobScheduling(Job arr[], int n) {
-
-      sort(arr, arr + n, comparison);
-      int maxi = arr[0].dead;
-      for (int i = 1; i < n; i++) {
-         maxi = max(maxi, arr[i].dead);
-      }
-
-      int slot[maxi + 1];
-
-      for (int i = 0; i <= maxi; i++)
-         slot[i] = -1;
-
-      int countJobs = 0, jobProfit = 0;
-
-      for (int i = 0; i < n; i++) {
-         for (int j = arr[i].dead; j > 0; j--) {
-            if (slot[j] == -1) {
-               slot[j] = i;
-               countJobs++;
-               jobProfit += arr[i].profit;
-               break;
-            }
-         }
-      }
-
+    
     //Function to find the maximum profit and the number of jobs done.
-    // static bool comparator(Job a,Job b){
-    //     return(a.profit>b.profit);
-    // }
-    // vector<int> JobScheduling(Job arr[], int n) 
-    // { 
-    //     sort(arr,arr+n,comparator);
-    //     int maxi=arr[0].dead;
-    //     for(int i=0;i<n;i++){
-    //         maxi=max(maxi,arr[i].dead);
-    //     }
-    //     int slot[maxi+1];
-    //     for(int i=0;i<maxi;i++){
-    //         slot[i]=-1;
-    //     }
-    //     int countJobs=0,jobProfit=0;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=arr[i].dead;j>0;j--){
-    //             if(slot[j]==-1){
-    //                 slot[j]=i;
-    //                 countJobs++;
-    //                 jobProfit+=arr[i].profit;
-    //                 break;
-    //             }
-    //         }
-    //     }
+    static bool comparator(Job a,Job b){
+        return(a.profit>b.profit);
+    }
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        sort(arr,arr+n,comparator);
+        int maxi=arr[0].dead;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,arr[i].dead);
+        }
+        int slot[maxi+1];
+        for(int i=0;i<=maxi;i++){
+            slot[i]=-1;
+        }
+        int countJobs=0,jobProfit=0;
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].dead;j>0;j--){
+                if(slot[j]==-1){
+                    slot[j]=i;
+                    countJobs++;
+                    jobProfit+=arr[i].profit;
+                    break;
+                }
+            }
+        }
         vector<int> ans;
         ans.push_back(countJobs);
         ans.push_back(jobProfit);
