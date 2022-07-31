34ef2778d88e6a7aa5af0e454de93193639da7db
Harshill Kumar Dhanjal <90548564+harshiilll@users.noreply.github.com>
2022-07-31 17:57:06 +0530

    Time: 3 ms (60.75%), Space: 8.3 MB (74.75%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,29 +1,43 @@
-class Solution{
-    public:
-    vector < int > inorderTraversal(TreeNode * root) {
-  vector < int > inorder;
-
-  TreeNode * cur = root;
-  while (cur != NULL) {
-    if (cur -> left == NULL) {
-      inorder.push_back(cur -> val);
-      cur = cur -> right;
-    } else {
-      TreeNode * prev = cur -> left;
-      while (prev -> right != NULL && prev -> right != cur) {
-        prev = prev -> right;
-      }
-
-      if (prev -> right == NULL) {
-        prev -> right = cur;
-        cur = cur -> left;
-      } else {
-        prev -> right = NULL;
-        inorder.push_back(cur -> val);
-        cur = cur -> right;
-      }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* curr=root;
+        vector<int> inorder;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right != NULL && prev->right !=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        
+        
+        return inorder;
+        
     }
-  }
-  return inorder;
-}
 };
