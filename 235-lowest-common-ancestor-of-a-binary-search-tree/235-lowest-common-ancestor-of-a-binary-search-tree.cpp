ad6ad14288283b46d90b05ad3372b771db599b20
Harshill Kumar Dhanjal <90548564+harshiilll@users.noreply.github.com>
2022-08-17 10:23:40 +0530

    Time: 24 ms (98.98%), Space: 23.2 MB (65.03%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,28 +9,17 @@
  */
 
 class Solution {
-    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& lca) {
-        if(!root || lca) return false;
-        
-        bool left = dfs(root->left, p, q, lca);
-        bool right = dfs(root->right, p, q, lca);
-        
-        bool cur = false;
-        if(root == p || root == q) {
-            cur = true;
-        }
-        if(left + right + cur >= 2) {
-            lca = root;
-            return false;
-        }
-        return left || right || cur;   
-    }
+    
     
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* ans = nullptr;
-        dfs(root, p, q, ans);
-        
-        return ans;
+        if(!root) return NULL;
+        int curr=root->val;
+        if(curr < p->val && curr < q->val)
+            return lowestCommonAncestor(root->right,p,q);
+        if(curr > p->val && curr > q->val)
+            return lowestCommonAncestor(root->left,p,q);
+        //else case(anwser)
+        return root;
     }
 };
